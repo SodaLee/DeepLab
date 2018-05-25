ea50ab56c39f7a9ab807e57892fa0ce304321fe2
LiuTed <1500012923@pku.edu.cn>
2018-05-25 14:03:10 +0800

    modify

--- deeplab.py
+++ deeplab.py
@@ -1,38 +1,56 @@
 import tensorflow as tf
-from resnet import resnet_50, resnet_101
+from resnet import *
 
-def ASPP(x, channel_out, dilations, name):
-	out = []
-	with tf.name_scope(name):
-		for i, d in enumerate(dilations):
-			out.append(conv_layer(x, 3, channel_out, "conv%d"%(i+1), rate = d))
-		out.append(conv_layer(x, 1, channel_out, "conv%d"%(len(dilations)+1)))
-		return tf.concat(out, -1)
-		
-def upsample(inputs, scale, name = None):
-	with tf.name_scope(name):
-		channel = inputs.shape()[-1]
-		f = tf.Variable(tf.truncated_normal([scale+1, scale+1, channel, channel], stddev = 0.1), "filter")
-		deconv = tf.nn.conv2d_transpose(inputs, f, tf.shape(inputs), [1,scale,scale,1], padding = "SAME", name = "deconvolution")
-		return deconv
+class deeplab_v3_plus(object):
+	def __init__(self, x, aspp_channels, decoder_channels, dense_out = 0):
+		self.dense_out = dense_out
+		self.dcnn, self.aspp = encoder(x, aspp_channels[0], aspp_channels[1], "encoder")
+		self.net = decoder(dcnn, aspp, decoder_channels, "decoder")
 
-def encoder(x, aspp_channel1, aspp_channel2, name):
-	with tf.name_scope(name):
-		dcnn = resnet_50(x)
-		aspp = ASPP(dcnn, aspp_channel1, [6, 12, 18, 24], "ASPP")
-		aspp = conv_layer(aspp, 1, aspp_channel2, "conv")
-		return dcnn, aspp
+	def _ASPP(self, x, channel_out, dilations, name):
+		out = []
+		with tf.name_scope(name):
+			for i, d in enumerate(dilations):
+				out.append(conv_layer(x, 3, channel_out, "conv%d"%(i+1), rate = d))
+			out.append(conv_layer(x, 1, channel_out, "conv%d"%(len(dilations)+1)))
+			return tf.concat(out, -1)
+			
+	def _upsample(self, inputs, scale, name):
+		with tf.name_scope(name):
+			channel = inputs.shape()[-1]
+			f = tf.Variable(tf.truncated_normal([scale+1, scale+1, channel, channel], stddev = 0.1), "filter")
+			deconv = tf.nn.conv2d_transpose(inputs, f, tf.shape(inputs), [1,scale,scale,1], padding = "SAME", name = "deconvolution")
+			return deconv
+
+	def _encoder(self, x, aspp_channel1, aspp_channel2, name):
+		with tf.name_scope(name):
+			dcnn = resnet_50(x)
+			if self.dense_out != 0:
+				self.dense = tf.reduce_mean(dcnn, axis = [1, 2])
+				self.dense = denes_layer(self.dense, self.dense_out)
+			aspp = self._ASPP(dcnn, aspp_channel1, [6, 12, 18, 24], "ASPP")
+			aspp = conv_layer(aspp, 1, aspp_channel2, "conv")
+			return dcnn, aspp
 
 
-def decoder(dcnn, aspp, channels, name):
-	with name_scope(name):
-		conv1 = conv_layer(dcnn, 1, channels[0], "conv1")
-		conv = tf.concat([conv1, upsample(dcnn, 2, "upsample1")], -1)
-		for i, c in enumerate(channels, 1):
-			conv = conv_layer(conv, 3, c, "conv%d"%(i+1))
-		conv = upsample(conv, 2, "upsample2")
-		return conv
+	def _decoder(self, dcnn, aspp, channels, name):
+		with name_scope(name):
+			conv1 = conv_layer(dcnn, 1, channels[0], "conv1")
+			conv = tf.concat([conv1, upsample(dcnn, 2, "upsample1")], -1)
+			for i, c in enumerate(channels, 1):
+				conv = conv_layer(conv, 3, c, "conv%d"%(i+1))
+			conv = self._upsample(conv, 2, "upsample2")
+			return conv
 
-def deeplab_v3_plus(x, channels, aspp_channels):
-	dcnn, aspp = encoder(x, aspp_channels[0], aspp_channels[1], "encoder")
-	return decoder(dcnn, aspp, channels, "decoder")
+	def get_pred(self):
+		return self.net
+
+	def get_dense(self):
+		return self.dense
+'''
+sample:
+deeplab = deeplab_v3_plus(x, [256, 256], [48, 512, 80], 100)
+net = deeplab.get_pred()
+resnet_train = deeplab.get_dense()
+...
+'''
--- resnet.py
+++ resnet.py
@@ -47,7 +47,7 @@ def atrous_residue_block(inputs, depth, channel_out, rate, name):
 		up = conv_layer(conv, 1, channel_out, "out", activate = None)
 		return tf.nn.relu6(up + inputs, name = "activation")
 
-def resnet(x, nconvs, name, dense_out = 0):
+def resnet(x, nconvs, name):
 	assert nconvs[0] == 1, "conv1 should only contain one convolution layer"
 	with tf.name_scope(name):
 		conv1 = conv_layer(x, 7, 64, "conv1", stride = [1,2,2,1])
@@ -63,15 +63,10 @@ def resnet(x, nconvs, name, dense_out = 0):
 			for j in range(nconvs[i]):
 				conv = atrous_residue_block(conv, depth, depth * 4, 2 * (i - len(nconvs) + 3), "conv%d_%d"%(i+1,j+1))
 			depth *= 2
-		if dense_out > 0:
-			avg = tf.reduce_mean(conv, axis = [1, 2], name = "global average pooling")
-			dense = dense_layer(avg, dense_out, "dense")
-			return conv, dense
-		else:
-			return conv
+		return conv
 
-def resnet_50(x, dense_out = 0):
-	return resnet(x, [1, 3, 4, 6, 3], "ResNet-50", dense_out)
+def resnet_50(x):
+	return resnet(x, [1, 3, 4, 6, 3], "ResNet-50")
 
 def resnet_101(x, dense_out = 0):
-	return resnet(x, [1, 3, 4, 23, 3], "ResNet-101", dense_out)
+	return resnet(x, [1, 3, 4, 23, 3], "ResNet-101")
