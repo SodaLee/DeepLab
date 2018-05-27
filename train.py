9a55c90db76b0c9b2931be4dcfa94543cfb72ed1
SodaLee <sodalee@pku.edu.cn>
2018-05-27 14:28:19 +0800

    debug train

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ from PythonAPI.prepare_data import prepare_dataset
 import deeplab
 import tensorflow as tf
 from PythonAPI.pycocotools.coco import COCO
+batch_size = 16
+num_classes = 80
 
 def main(train_type='Resnet', restore=False, maxiter=10, test=False):
 	train_annFile = './annotations/instances_train2017.json'
@@ -21,22 +23,9 @@ def main(train_type='Resnet', restore=False, maxiter=10, test=False):
 	next_res_val = res_val_iter.get_next()
 	next_deep_val = deep_val_iter.get_next()
 
-	#_imgs = tf.placeholder(tf.float32, [batch_size, None, None, 3])
-	#_labels = tf.placeholder(tf.float32, [batch_size, num_classes])
-	#_gt = tf.placeholder(tf.float32, [batch_size, None, None, num_classes])
-	training = tf.placeholder(tf.bool)
-	res_end = tf.placeholder(tf.bool)
-
-	if res_end:
-		if training:
-			_imgs, _labels = next_res_train
-		else:
-			_imgs, _labels = next_res_val
-	else:
-		if training:
-			_imgs, _gt = next_deep_train
-		else:
-			_imgs, _gt = next_deep_val
+	_imgs = tf.placeholder(tf.float32, [batch_size, None, None, 3])
+	_labels = tf.placeholder(tf.float32, [batch_size, num_classes])
+	_gt = tf.placeholder(tf.float32, [batch_size, None, None, num_classes])
 
 	_deeplab = deeplab.deeplab_v3_plus(_imgs, [128, 64], [48, num_classes], num_classes)
 	res_out = _deeplab.get_dense()
@@ -65,7 +54,8 @@ def main(train_type='Resnet', restore=False, maxiter=10, test=False):
 				sess.run(res_train_iter.initializer)
 				while True:
 					try:
-						_, _loss = sess.run([res_op, res_mean_loss], feed_dict={training: True, res_end: True})
+						imgs, labels = sess.run(next_res_train)
+						_, _loss = sess.run([res_op, res_mean_loss], feed_dict={_imgs: imgs, _labels: labels})
 						total_loss += _loss
 						cnt += 1
 						if cnt % 25 == 0:
@@ -80,7 +70,8 @@ def main(train_type='Resnet', restore=False, maxiter=10, test=False):
 				sess.run(res_val_iter.initializer)
 				while True:
 					try:
-						_, _loss = sess.run([res_op, res_mean_loss], feed_dict={training: False, res_end: True})
+						imgs, labels = sess.run(next_res_val)
+						_, _loss = sess.run([res_op, res_mean_loss], feed_dict={_imgs: imgs, _labels: labels})
 						total_loss += _loss
 						cnt += 1
 						if cnt % 25 == 0:
@@ -90,10 +81,44 @@ def main(train_type='Resnet', restore=False, maxiter=10, test=False):
 						break
 
 				saver.save(sess, "./model/model.ckpt")
-				saver.save(sess, "./model/model_%d.ckpt" % i)
+				saver.save(sess, "./model/model_res_%d.ckpt" % i)
 				print('model saved')
 		elif train_type == 'Deeplab':
-			pass
+			for i in list(range(maxiter)):
+				cnt = 0
+				total_loss = 0.0
+				sess.run(res_train_iter.initializer)
+				while True:
+					try:
+						imgs, gt = sess.run(next_deep_train)
+						_, _loss = sess.run([res_op, res_mean_loss], feed_dict={_imgs: imgs, _gt: gt})
+						total_loss += _loss
+						cnt += 1
+						if cnt % 25 == 0:
+							print(cnt * 16, total_loss / 25)
+							total_loss = 0.0
+					except tf.errors.OutOfRangeError:
+						break
+				print('epoc %d done' % (i + 1))
+
+				cnt = 0
+				total_loss = 0.0
+				sess.run(res_val_iter.initializer)
+				while True:
+					try:
+						imgs, gt = sess.run(next_deep_val)
+						_, _loss = sess.run([res_op, res_mean_loss], feed_dict={_imgs: imgs, _gt: gt})
+						total_loss += _loss
+						cnt += 1
+						if cnt % 25 == 0:
+							print(cnt * 16, total_loss / 25)
+							total_loss = 0.0
+					except tf.errors.OutOfRangeError:
+						break
+
+				saver.save(sess, "./model/model.ckpt")
+				saver.save(sess, "./model/model_deep_%d.ckpt" % i)
+				print('model saved')
 		else:
 			pass
 
